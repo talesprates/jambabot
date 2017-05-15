f75d6bca7f8e83a406714d8b246c7c5d2d91fd6d
Tales Prates Correia <tcorreia@daitangroup.com>
2017-05-15 08:09:18 -0300

    hardcoded userId on MailManager

--- integrations/gmail/MailManager.js
+++ integrations/gmail/MailManager.js
@@ -58,18 +58,18 @@ const gmail = google.gmail('v1');
         }
 
         resolve(
-          Promise.all(response.messages.map(message => fetchMessage(oauth2Client, 'me', message)))
+          Promise.all(response.messages.map(message => fetchMessage(oauth2Client, message)))
         );
       });
     });
   }
 
 
-  function fetchMessage(oauth2Client, userId, message) {
+  function fetchMessage(oauth2Client, message) {
     return new Promise((resolve, reject) => {
       gmail.users.messages.get({
         auth: oauth2Client,
-        userId, // TODO it is really mandatory?
+        userId: 'me',
         id: message.id,
       }, (error, response) => {
         if (error) {
